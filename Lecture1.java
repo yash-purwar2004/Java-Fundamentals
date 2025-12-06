79ee19e8026d8446ca453b2312f55b2fd20a6d1b
yash-purwar2004 <nayan251198@gmail.com>
2025-12-06 19:44:31 +0530

    Understand the skeleton of java program

---
+++ Lecture1.java
@@ -0,0 +1,7 @@
+class Lecture1{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
